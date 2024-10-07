81bb43c2b5db78a3ca96f9a01f24ae2e752b6834
wareverdud <71446364+wareverdud@users.noreply.github.com>
2024-10-08 02:27:47 +0300

    add delete languages

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import { useState, useEffect } from 'react'
 import translate from 'translate'
 import { languages } from './lib'
+import trashIcon from './assets/trash.svg'
 import './App.css'
 
 export const App = () => {
   const [value, setValue] = useState('')
   const [initLanguage, setInitLanguage] = useState('en')
-  const [targetLanguages, setTargetLanguages] = useState<string[]>(['it', 'es'])
+  const [targetLanguages, setTargetLanguages] = useState<string[]>(['it'])
+  const [loading, setLoading] = useState(false)
+  const [debounceTimeout, setDebounceTimeout] = useState<NodeJS.Timeout | null>(
+    null,
+  )
   const [translations, setTranslations] = useState<{ [key: string]: string }>(
     targetLanguages.reduce(
       (acc: { [key: string]: string }, language: string) => {
@@ -15,10 +20,6 @@ export const App = () => {
       },
       {},
     ),
-  )
-  const [loading, setLoading] = useState(false)
-  const [debounceTimeout, setDebounceTimeout] = useState<NodeJS.Timeout | null>(
-    null,
   )
 
   const handleTranslate = async (
@@ -71,34 +72,31 @@ export const App = () => {
   const handleAddLanguage = (newLang: string) => {
     if (!targetLanguages.includes(newLang)) {
       setTargetLanguages((prev) => [...prev, newLang])
-      setTranslations((prev) => ({ ...prev, [newLang]: '' }))
+    }
+  }
+
+  const handleDeleteLanguage = (lang: string) => {
+    if (targetLanguages.includes(lang)) {
+      setTargetLanguages((prev) => prev.filter((l) => l !== lang))
     }
   }
 
   return (
-    <div className="container">
-      <div className="language-block">
-        <select
-          onChange={(e) => setInitLanguage(e.target.value)}
-          value={initLanguage}
-        >
+    <>
+      <div className="header">
+        <select onChange={(e) => handleAddLanguage(e.target.value)}>
           {languages.map((item) => (
             <option key={item.id} value={item.id}>
               {item.label}
             </option>
           ))}
         </select>
-        <textarea
-          placeholder="Enter your text"
-          value={value}
-          onChange={(e) => setValue(e.target.value)}
-        />
       </div>
-      {Object.entries(translations).map(([lang, translatedText]) => (
-        <div className="language-block" key={lang}>
+      <div className="source">
+        <div className="language-block">
           <select
-            value={lang}
-            onChange={(e) => handleAddLanguage(e.target.value)}
+            onChange={(e) => setInitLanguage(e.target.value)}
+            value={initLanguage}
           >
             {languages.map((item) => (
               <option key={item.id} value={item.id}>
@@ -106,17 +104,45 @@ export const App = () => {
               </option>
             ))}
           </select>
-          <div>
-            {loading ? (
-              <div className="spinner"></div>
-            ) : (
-              <textarea value={translatedText} readOnly />
-            )}
-          </div>
+          <textarea
+            placeholder="Enter your text"
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+          />
         </div>
-      ))}
-      <button onClick={() => handleAddLanguage('fr')}>Add French</button>
-      <button onClick={() => handleAddLanguage('ru')}>Add Russian</button>
-    </div>
+      </div>
+      <div className="container">
+        {Object.entries(translations).map(([lang, translatedText]) => (
+          <div className="language-block" key={lang}>
+            <div className="action">
+              <select
+                value={lang}
+                onChange={(e) => handleAddLanguage(e.target.value)}
+              >
+                {languages.map((item) => (
+                  <option key={item.id} value={item.id}>
+                    {item.label}
+                  </option>
+                ))}
+              </select>
+              <img
+                className="trashIcon"
+                src={trashIcon}
+                alt="Trash Icon"
+                onClick={() => handleDeleteLanguage(lang)}
+              />
+            </div>
+
+            <div>
+              {loading ? (
+                <div className="spinner"></div>
+              ) : (
+                <textarea value={translatedText} readOnly />
+              )}
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
   )
 }
