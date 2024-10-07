0a5d12524bbb19c2760c6f14836e4ec1adfc5f70
wareverdud <71446364+wareverdud@users.noreply.github.com>
2024-10-08 01:00:44 +0300

    basic translate

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,96 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
+import translate from 'translate'
+import { languages } from './lib'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+export const App = () => {
+  const [value, setValue] = useState('')
+  const [initLanguage, setInitLanguage] = useState('en')
+  const [translation, setTranslation] = useState('')
+  const [targetLanguage, setTargetLanguage] = useState('it')
+  const [loading, setLoading] = useState(false)
+  const [debounceTimeout, setDebounceTimeout] = useState<NodeJS.Timeout | null>(
+    null,
+  )
+
+  const handleTranslate = async (
+    text: string,
+    options: { from: string; to: string },
+  ) => {
+    const result = await translate(text, options)
+    return result
+  }
+
+  useEffect(() => {
+    if (debounceTimeout) {
+      clearTimeout(debounceTimeout)
+    }
+
+    const newTimeout = setTimeout(async () => {
+      if (value.trim()) {
+        setLoading(true)
+        const translatedText = await handleTranslate(value, {
+          from: initLanguage,
+          to: targetLanguage,
+        })
+        setTranslation(translatedText)
+        setLoading(false)
+      } else {
+        setTranslation('')
+      }
+    }, 500)
+
+    setDebounceTimeout(newTimeout)
+
+    return () => clearTimeout(newTimeout)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [value, initLanguage, targetLanguage])
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container">
+      <div className="language-block">
+        <select
+          onChange={(e) => setInitLanguage(e.target.value)}
+          value={initLanguage}
+        >
+          {languages.map((item) => (
+            <option key={item.id} value={item.id}>
+              {item.label}
+            </option>
+          ))}
+        </select>
+        <textarea
+          placeholder="Enter your text"
+          value={value}
+          onChange={(e) => {
+            if (
+              e.target.value.trim() !== ' ' ||
+              e.target.value.trim() !== '\n'
+            ) {
+              setValue(e.target.value)
+            }
+          }}
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="language-block">
+        <select
+          onChange={(e) => setTargetLanguage(e.target.value)}
+          value={targetLanguage}
+        >
+          {languages.map((item) => (
+            <option key={item.id} value={item.id}>
+              {item.label}
+            </option>
+          ))}
+        </select>
+        <div>
+          {loading ? (
+            <div className="spinner"></div>
+          ) : (
+            <textarea value={translation} readOnly />
+          )}
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
-
-export default App
